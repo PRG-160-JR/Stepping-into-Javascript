3b7d1ce45b0e2c7c7dd5a5a25a4bfac5cc4f149e
jrissler9 <jrissler9@gmail.com>
2018-02-11 17:39:42 -0500

    added a function to calculate the average of 5 numbers to test.js

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-  function getAverage (a,b,c,d,e,f){
+  function getAverage (a,b,c,d,e){
 
-      var average = (a + b + c + d + e + f) / 6;
+      var average = (a + b + c + d + e) / 5;
       console.log(average);
       return average;
 
   }
 
-var myResult =  getAverage(7,8,9,10,11,12);
+var myResult =  getAverage(7,2,5,20,13);
  console.log("the average is " + myResult);
